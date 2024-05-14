328d03a803dc718ebb288b4a4a999fc377106d8b
Patocv14 <PatricioCervantes140207@hotmail.com>
2024-05-13 21:01:31 -0600

    se fixea return de endpoint

--- src/domain/use-cases/auth/verify-token.use-case.ts
+++ src/domain/use-cases/auth/verify-token.use-case.ts
@@ -1,7 +1,10 @@
 import { AuthRepository } from "../../repositories";
+import { UserMapper } from "../../../infrestructure";
+import { UserEntity } from "../../entities";
+import { CustomErrors } from "../../errors/custom.error";
 
 interface VerifyToken {
-  execute(token: string): Promise<boolean>
+  execute(token: string): Promise<UserEntity>
 }
 
 export class VerifyTokenUseCase implements VerifyToken {
@@ -10,10 +13,10 @@ export class VerifyTokenUseCase implements VerifyToken {
     private readonly authRepository: AuthRepository,
   ){}
 
-  async execute(token: string): Promise<boolean> {
+  async execute(token: string): Promise<UserEntity> {
     const user = await this.authRepository.verifyAccessToken(token);
-    if(!user) return false;
-    return true;
+    if(!user) throw CustomErrors.badRequest("Invalid token");
+    return UserMapper.userEntityFromObject(user);
   }
 
 }
